a59aeb1b14f43f2eae1820f314b9063d807d8575
Pinxuan Huang <pinxhuang@gmail.com>
2023-11-16 14:47:03 -0500

    Make home page more presentable

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,32 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="page">
+	<h1>Gachadle</h1>
+	<p>Upcoming casual gacha game under development</p>
+</div>
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Kdam+Thmor+Pro&family=Open+Sans:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&display=swap');
+	.page {
+		text-align: center;
+		align-items: center;
+		background-color: #040306;
+		color: rgb(115, 96, 212);
+		font-family: 'Kdam Thmor Pro', sans-serif;
+		width: 100vw;
+		height: 100vh;
+		margin: auto;
+		display: table-cell;
+		vertical-align: middle;
+	}
+	.page ::selection {
+		background-color: magenta;
+		color: cyan;
+	}
+	.page h1 {
+		font-size: 8em;
+		text-transform: uppercase;
+		margin: 0;
+	}
+	.page p {
+		font-size: 1em;
+	}
+</style>
